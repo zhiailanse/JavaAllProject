f1cb48fb990cc8d5db24d450a24f90220ab0b2c4
zhiailanse <zhiailanse_1@163.com>
2014-04-08 00:46:15 +0800

    commit

--- JavaAllProject/src/com/zmm/java/dp/commend/copy/Command.java
+++
@@ -1,7 +0,0 @@
-package com.zmm.java.dp.commend.copy;
-
-public abstract class Command {
-	abstract void execute();
-	
-	abstract void undo();
-}
--- JavaAllProject/src/com/zmm/java/dp/state/copy/MMState.java
+++
@@ -1,7 +0,0 @@
-package com.zmm.java.dp.state.copy;
-
-public interface MMState {
-	void cry();
-	void smile();
-	void sleep();
-}
